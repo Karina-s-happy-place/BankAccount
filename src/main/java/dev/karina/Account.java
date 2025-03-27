f25cca353152896199d1cb042c6a8c3f61cb917a
Karina Lorenzo <karinalrg16@gmail.com>
2025-03-27 11:52:38 +0100

    feat: class Account and methods added

---
+++ src/main/java/dev/karina/Account.java
@@ -0,0 +1,29 @@
+package dev.karina;
+
+public class Account {
+
+    protected float balance;
+    protected int depositCount = 0;
+    protected int withdrawCount = 0;
+    protected float annualInterestRate;
+    protected float monthlyFee = 0.0f;
+
+    public Account(float balance, float annualInterestRate) {
+        this.balance = balance;
+        this.annualInterestRate = annualInterestRate;
+    }
+
+    public void deposit(float ammount) {
+        balance += ammount;
+        depositCount++;
+    }
+
+    public float getBalance() {
+        return balance;
+    }
+
+    public int getDepositCount() {
+        return depositCount;
+    }
+
+}
