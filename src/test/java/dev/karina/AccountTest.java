55c87173beb9385cfa21fa30fe8ff41212160fca
Karina Lorenzo <karinalrg16@gmail.com>
2025-03-27 12:06:25 +0100

    feat: testWidthdraw implemented. Test failed

--- src/test/java/dev/karina/AccountTest.java
+++ src/test/java/dev/karina/AccountTest.java
@@ -12,4 +12,15 @@ public class AccountTest {
         assertEquals(7000.0f, account.getBalance(), 0.001, "Balance should be update correctly");
         assertEquals(1, account.getDepositCount(), "Deposit count should be incremented");
     }
+
+    @Test
+    public void testWidthdraw() {
+        Account account = new Account(5000.0f, 5.0f);
+        account.withdraw(3000.0f);
+        assertEquals(2000.0f, account.getBalance(), 0.001, "Balance should be decrease correctly");
+        assertEquals(1, account.getWithdrawalCount(), "Withdrawal count should be incremented");
+        account.withdraw(4000.0f);
+        assertEquals(2000, account.getWithdrawalCount(), "Withdrawal count should be incremented on overdraw");
+
+    }
 }
