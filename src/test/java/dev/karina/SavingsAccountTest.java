dee0890209a1f030dbc5424506bd4d1cd48c8bc7
Karina Lorenzo <karinalrg16@gmail.com>
2025-03-27 18:36:18 +0100

    feat- testActivation implemented. Test failed

---
+++ src/test/java/dev/karina/SavingsAccountTest.java
@@ -0,0 +1,19 @@
+package dev.karina;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class SavingsAccountTest {
+    @Test
+    public void testActivation() {
+        SavingsAccount account = new SavingsAccount(8000.0f, 5.0f);
+        assertFalse(account.isActive(), "Account should be inactive");
+
+        account.deposit(3000.0f);
+        assertTrue(account.isActive(), "Account should be activated upon reaching a balance >= 10,000.");
+        assertEquals(11000.0f, account.getBalance(), "The balance should be updated correctly after depositing.");
+    }
+}
