27c31eacdce49e87f1fcdc661f7b68bc85774117
Karina Lorenzo <karinalrg16@gmail.com>
2025-03-27 19:22:27 +0100

    feat: testWithdrawWhenActive implemented. Test failed

--- src/test/java/dev/karina/SavingsAccountTest.java
+++ src/test/java/dev/karina/SavingsAccountTest.java
@@ -16,4 +16,12 @@ public class SavingsAccountTest {
         assertTrue(account.isActive(), "Account should be activated upon reaching a balance >= 10,000.");
         assertEquals(11000.0f, account.getBalance(), "The balance should be updated correctly after depositing.");
     }
+
+    @Test
+    public void testWithdrawWhenActive() {
+        SavingsAccount account = new SavingsAccount(12000.0f, 5.0f);
+        account.withdraw(3000.0f);
+        assertEquals(9000.0f, account.getBalance(), "the balance should reflect the withdrawal");
+        assertFalse(account.isActive(), "Account should be inactive with balance < 10,0000.");
+    }
 }
