6587a0c553d66780516e2b390b53ba595a64de33
Karina Lorenzo <karinalrg16@gmail.com>
2025-03-27 19:36:26 +0100

    feat: method withdraw implemented. Test passed

--- src/main/java/dev/karina/SavingsAccount.java
+++ src/main/java/dev/karina/SavingsAccount.java
@@ -17,4 +17,16 @@ public class SavingsAccount extends Account {
     public boolean isActive() {
         return active;
     }
+
+    @Override
+    public void withdraw(float amount) {
+        System.out.println("Before withdraw: " + balance + ", Active status:" + active);
+        if (active) {
+            super.withdraw(amount);
+        } else {
+            System.out.println("Account is inactive, cannot withdraw");
+        }
+        this.active = balance >= 10000.0f;
+        System.out.println("After withdraw:" + balance + ", Active statuts:" + active);
+    }
 }
